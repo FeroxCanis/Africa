886bfa46ff6d07f2fe4299df062d772c0eeaf35d
soroush <soroushjorjorzade@gmail.com>
2022-02-05 10:16:03 +0330

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Ferox on 2/5/22.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
